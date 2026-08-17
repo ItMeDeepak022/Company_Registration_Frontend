826c293e56663ab04b59c1cd1716246ecc52013a
Deepak <deepakkushwaha5945@gmail.com>
2026-08-17 16:42:59 +0530

    updated footer

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className=" bg-white fixed bottom-0 left-0 right-0 text-center border-t-1 border-gray-200 text-balck sm:p-0 p-1  sm:py-6 shrink-0">
+    <footer className=" bg-white fixed bottom-0 left-0 right-0 text-center border-t-1 border-gray-200 text-balck sm:p-0 p-3  sm:py-6 shrink-0">
       <p>&copy; {new Date().getFullYear()} Company Registration &amp; Verification. All rights reserved.</p>
     </footer>
   );
